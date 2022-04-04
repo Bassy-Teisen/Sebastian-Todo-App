de7b100ef9c9dde9c9a059aab35254d6738f4abc
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-04-05 09:15:19 +1000

    created components repo and header component with centered h1

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+
+const Header = () => {
+    
+
+    return (
+        <header className="header">
+            <h1>Todo App</h1>
+        </header>
+    )
+
+}
+
+export default Header 
